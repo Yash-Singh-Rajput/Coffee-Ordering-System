3418219742196cce11ff66c46a7b9cb2ede7d88b
Yash Digvijay Singh <yash.singh112802@marwadiuniversity.ac.in>
2025-02-08 11:46:30 +0530

    Update Coffee.sol

--- Coffee.sol
+++ Coffee.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract coffee{
+
+    struct Memo{
+        string name;
+        string message;
+        uint timestamp;
+        address from;
+    }
+
+    Memo[] memos;
+    address payable owner; //owner is going to receive funds
+    constructor(){
+        owner = payable(msg.sender);
+    }
+
+    
+}
